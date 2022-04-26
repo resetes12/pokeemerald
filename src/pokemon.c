b12d0e1b59533d1470573eb78ad3e13cebefb6f9
TheXaman <48356183+TheXaman@users.noreply.github.com>
2022-04-26 23:04:14 +0200

    fix for level cap effecting enemy trainer mons

--- src/pokemon.c
+++ src/pokemon.c
@@ -9437,7 +9437,7 @@ u8 GetLevelFromMonExp(struct Pokemon *mon)
     u32 exp = GetMonData(mon, MON_DATA_EXP, NULL);
     s32 level = 1;
 
-    while (level <= GetCurrentPartyLevelCap() && gExperienceTables[gBaseStats[species].growthRate][level] <= exp)
+    while (level <= MAX_LEVEL && gExperienceTables[gBaseStats[species].growthRate][level] <= exp)
         level++;
 
     return level - 1;
@@ -9449,7 +9449,7 @@ u8 GetLevelFromBoxMonExp(struct BoxPokemon *boxMon)
     u32 exp = GetBoxMonData(boxMon, MON_DATA_EXP, NULL);
     s32 level = 1;
 
-    while (level <= GetCurrentPartyLevelCap() && gExperienceTables[gBaseStats[species].growthRate][level] <= exp)
+    while (level <= MAX_LEVEL && gExperienceTables[gBaseStats[species].growthRate][level] <= exp)
         level++;
 
     return level - 1;
