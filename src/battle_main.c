c8f3ff504c7cea8e0f05bc14274de1c3b3c83da2
TheXaman <48356183+TheXaman@users.noreply.github.com>
2022-05-05 22:56:36 +0200

    nuzlocke pokemon fainting not active in special battles including Steven battles

--- src/battle_main.c
+++ src/battle_main.c
@@ -5232,7 +5232,14 @@ static void HandleEndTurn_FinishBattle(void)
         //ty_difficulty_challenges
         if (IsNuzlockeActive())
         {
-            NuzlockeDeleteFaintedPartyPokemon();
+            if (!(gBattleTypeFlags &(BATTLE_TYPE_LINK
+                                        | BATTLE_TYPE_LINK_IN_BATTLE
+                                        | BATTLE_TYPE_FIRST_BATTLE
+                                        | BATTLE_TYPE_WALLY_TUTORIAL
+                                        | BATTLE_TYPE_INGAME_PARTNER
+                                        | BATTLE_TYPE_TOWER_LINK_MULTI
+                                        | BATTLE_TYPE_RECORDED_LINK)))
+                NuzlockeDeleteFaintedPartyPokemon();
             if (!(gBattleTypeFlags & (BATTLE_TYPE_DOUBLE
                                         | BATTLE_TYPE_LINK
                                         | BATTLE_TYPE_TRAINER
