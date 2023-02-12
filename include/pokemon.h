4d7d864f01a0cb0114f30c6b5868750d6de02dca
resetes12 <diploporigon@gmail.com>
2023-02-12 10:21:15 +0100

    Phy/SP Split

--- include/pokemon.h
+++ include/pokemon.h
@@ -317,6 +317,10 @@ struct SpeciesInfo
             u8 noFlip : 1;
 };
 
+#define MOVE_CATEGORY_PHYSICAL 0
+#define MOVE_CATEGORY_SPECIAL 1
+#define MOVE_CATEGORY_STATUS 2
+
 struct BattleMove
 {
     u8 effect;
@@ -328,6 +332,7 @@ struct BattleMove
     u8 target;
     s8 priority;
     u8 flags;
+    u8 category;
 };
 
 #define SPINDA_SPOT_WIDTH 16
