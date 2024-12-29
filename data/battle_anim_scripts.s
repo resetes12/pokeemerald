7e290cfd38caddbdf479b30950800aef26a694db
resetes12 <diploporigon@gmail.com>
2024-12-29 10:19:43 +0100

    Reduced animations when healing (eg. leftovers)

--- data/battle_anim_scripts.s
+++ data/battle_anim_scripts.s
@@ -10448,12 +10448,12 @@ General_HeldItemEffect:
 	loadspritegfx ANIM_TAG_THIN_RING
 	loadspritegfx ANIM_TAG_SPARKLE_2
 	delay 0
-	playsewithpan SE_M_TAKE_DOWN, SOUND_PAN_ATTACKER
-	createvisualtask AnimTask_RotateMonToSideAndRestore, 2, 16, 128, ANIM_ATTACKER, 2
-	waitforvisualfinish
-	playsewithpan SE_M_TAKE_DOWN, SOUND_PAN_ATTACKER
-	createvisualtask AnimTask_RotateMonToSideAndRestore, 2, 16, 128, ANIM_ATTACKER, 2
-	waitforvisualfinish
+	@playsewithpan SE_M_TAKE_DOWN, SOUND_PAN_ATTACKER
+	@createvisualtask AnimTask_RotateMonToSideAndRestore, 2, 16, 128, ANIM_ATTACKER, 2
+	@waitforvisualfinish
+	@playsewithpan SE_M_TAKE_DOWN, SOUND_PAN_ATTACKER
+	@createvisualtask AnimTask_RotateMonToSideAndRestore, 2, 16, 128, ANIM_ATTACKER, 2
+	@waitforvisualfinish
 	playsewithpan SE_M_TAKE_DOWN, SOUND_PAN_ATTACKER
 	createvisualtask AnimTask_RotateMonToSideAndRestore, 2, 16, 128, ANIM_ATTACKER, 2
 	waitforvisualfinish
