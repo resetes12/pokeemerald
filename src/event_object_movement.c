22a7569abfc8e793bac3ef608dedc6de4ec307c8
resetes12 <diploporigon@gmail.com>
2025-08-18 13:00:29 +0200

    Followers 73e891a: rainy castform is no longer a Ninja Boy on return-to-field

--- src/event_object_movement.c
+++ src/event_object_movement.c
@@ -3000,13 +3000,14 @@ const struct ObjectEventGraphicsInfo *GetObjectEventGraphicsInfo(u16 graphicsId)
     if (graphicsId >= OBJ_EVENT_GFX_VARS && graphicsId <= OBJ_EVENT_GFX_VAR_F)
         graphicsId = VarGetObjectEventGraphicsId(graphicsId - OBJ_EVENT_GFX_VARS);
 
-    if (graphicsId >= OBJ_EVENT_GFX_MON_BASE + SPECIES_SHINY_TAG)
-        graphicsId -= SPECIES_SHINY_TAG;
     // graphicsId may contain mon form info
     if (graphicsId > OBJ_EVENT_GFX_SPECIES_MASK) {
         form = graphicsId >> OBJ_EVENT_GFX_SPECIES_BITS;
         graphicsId = graphicsId & OBJ_EVENT_GFX_SPECIES_MASK;
     }
+    
+    if (graphicsId >= OBJ_EVENT_GFX_MON_BASE + SPECIES_SHINY_TAG)
+        graphicsId -= SPECIES_SHINY_TAG;
 
     if (graphicsId == OBJ_EVENT_GFX_BARD) {
         return gMauvilleOldManGraphicsInfoPointers[GetCurrentMauvilleOldMan()];
