aab6ca6c8237ba12e7e3c2552c93c3f2b065cd59
resetes12 <diploporigon@gmail.com>
2025-10-14 22:52:17 +0200

    Some fixes

--- src/pokemon.c
+++ src/pokemon.c
@@ -76,6 +76,7 @@ static void Task_PlayMapChosenOrBattleBGM(u8 taskId);
 static bool8 ShouldGetStatBadgeBoost(u16 flagId, u8 battlerId);
 static u16 GiveMoveToBoxMon(struct BoxPokemon *boxMon, u16 move);
 static bool8 ShouldSkipFriendshipChange(void);
+static u16 GetPreEvolution(u16 species);
 
 EWRAM_DATA static u8 sLearningMoveTableID = 0;
 EWRAM_DATA u8 gPlayerPartyCount = 0;
