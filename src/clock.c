b35d9874bea608546be0965a28b2580674b99d72
resetes12 <diploporigon@gmail.com>
2025-10-21 15:13:06 +0200

    Reversed "clock change" change; now time events work after RTC change

--- src/clock.c
+++ src/clock.c
@@ -63,13 +63,14 @@ static void UpdatePerMinute(struct Time *localTime)
 
     CalcTimeDifference(&difference, &gSaveBlock2Ptr->lastBerryTreeUpdate, localTime);
     minutes = 24 * 60 * difference.days + 60 * difference.hours + difference.minutes;
-    if (minutes != 0)
+    if (minutes > 0)
     {
-        if (minutes >= 0)
-        {
-            BerryTreeTimeUpdate(minutes);
-            gSaveBlock2Ptr->lastBerryTreeUpdate = *localTime;
-        }
+        BerryTreeTimeUpdate(minutes);
+        gSaveBlock2Ptr->lastBerryTreeUpdate = *localTime;
+    }
+    else if (minutes < 0)
+    {
+        gSaveBlock2Ptr->lastBerryTreeUpdate = *localTime;
     }
 }
 
