abdf383bb0cf66905d8e7a4e7de410f21d099994
resetes12 <diploporigon@gmail.com>
2025-10-28 09:51:51 +0100

    Reverted another AI change made long ago

--- src/battle_ai_switch_items.c
+++ src/battle_ai_switch_items.c
@@ -667,7 +667,6 @@ u8 GetMostSuitableMonToSwitchInto(void)
     s32 i, j;
     u8 invalidMons;
     u16 move;
-    bool8 checkedAllMonForSEMoves = FALSE;  // We have checked all Pokemon in the party for if they have a super effective move
 
     if (*(gBattleStruct->monToSwitchIntoId + gActiveBattler) != PARTY_SIZE)
         return *(gBattleStruct->monToSwitchIntoId + gActiveBattler);
@@ -715,8 +714,7 @@ u8 GetMostSuitableMonToSwitchInto(void)
 
     while (invalidMons != (1 << PARTY_SIZE) - 1) // All mons are invalid.
     {
-        //bestDmg = TYPE_MUL_NO_EFFECT;
-        bestDmg = 255;
+        bestDmg = TYPE_MUL_NO_EFFECT;
         bestMonId = PARTY_SIZE;
         // Find the mon whose type is the most suitable offensively.
         for (i = firstId; i < lastId; i++)
@@ -739,8 +737,7 @@ u8 GetMostSuitableMonToSwitchInto(void)
                 /* Possible bug: this comparison gives the type that takes the most damage, when
                 a "good" AI would want to select the type that takes the least damage. Unknown if this
                 is a legitimate mistake or if it's an intentional, if weird, design choice */
-                //if (bestDmg < typeDmg)
-                if (bestDmg > typeDmg)
+                if (bestDmg < typeDmg)
                 {
                     bestDmg = typeDmg;
                     bestMonId = i;
@@ -762,16 +759,10 @@ u8 GetMostSuitableMonToSwitchInto(void)
                     break;
             }
 
-            //if (i != MAX_MON_MOVES)
-            if (i != MAX_MON_MOVES || (checkedAllMonForSEMoves && bestDmg <= TYPE_MUL_NOT_EFFECTIVE))
+            if (i != MAX_MON_MOVES)
                 return bestMonId; // Has both the typing and at least one super effective move.
 
             invalidMons |= gBitTable[bestMonId]; // Sorry buddy, we want something better.
-            if (invalidMons == 0x3F && !checkedAllMonForSEMoves)  // If we already checked all for a super effective move, then use the one with the best typing
-            {
-                invalidMons = 0;
-                checkedAllMonForSEMoves = TRUE;
-            }
         }
         else
         {
