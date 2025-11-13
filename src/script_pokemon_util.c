88ee09f7f929d799e055879b074a7225adc1585a
resetes12 <diploporigon@gmail.com>
2025-11-13 19:24:29 +0100

    Fixed static randomization

--- src/script_pokemon_util.c
+++ src/script_pokemon_util.c
@@ -102,6 +102,12 @@ u8 ScriptGiveMon(u16 species, u8 level, u16 item, u32 unused1, u32 unused2, u8 u
         CreateMon(&mon, species, level, USE_RANDOM_IVS, FALSE, 0, OT_ID_PLAYER_ID, 0);
         FlagClear(FLAG_NO_SHINY);
     }
+    // if the static randomizer is on, and you got your first Pokémon already, givemons will be randomized
+    else if ((gSaveBlock1Ptr->tx_Random_Static) && (FlagGet(FLAG_SYS_POKEMON_GET) == TRUE))
+    {
+        species = GetSpeciesRandomSeeded(species, TX_RANDOM_T_STATIC, 0);
+        CreateMon(&mon, species, level, USE_RANDOM_IVS, FALSE, 0, OT_ID_PLAYER_ID, 0);
+    }
     else
         CreateMon(&mon, species, level, USE_RANDOM_IVS, FALSE, 0, OT_ID_PLAYER_ID, 0);
     heldItem[0] = item;
