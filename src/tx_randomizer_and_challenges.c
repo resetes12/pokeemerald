b21dc41fe3d0e91754bb9ad2b93ff51be73e93b0
resetes12 <diploporigon@gmail.com>
2025-12-08 14:45:54 +0100

    Added HM check to Nuzlocke easy mode

--- src/tx_randomizer_and_challenges.c
+++ src/tx_randomizer_and_challenges.c
@@ -114,6 +114,7 @@ bool8 IsPokecenterChallengeActivated(void)
 bool8 HMsOverwriteOptionActive(void)
 {
     return (gSaveBlock1Ptr->tx_Challenges_Nuzlocke 
+            || gSaveBlock1Ptr->tx_Nuzlocke_EasyMode 
             || gSaveBlock1Ptr->tx_Challenges_Mirror 
             || gSaveBlock1Ptr->tx_Random_Moves
             || IsOneTypeChallengeActive());
