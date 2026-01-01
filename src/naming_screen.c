aa7fc533d8d9eefa2a49a32dff319cd22fbace90
resetes12 <diploporigon@gmail.com>
2026-01-01 21:25:51 +0100

    Auto switch to lowercase after first letter

--- src/naming_screen.c
+++ src/naming_screen.c
@@ -1503,6 +1503,9 @@ static bool8 KeyboardKeyHandler_Character(u8 input)
     {
         bool8 textFull = AddTextCharacter();
 
+        if (sNamingScreen ->currentPage == KBPAGE_LETTERS_UPPER && GetTextEntryPosition() == 1)
+        MainState_StartPageSwap();
+
         SquishCursor();
         if (textFull)
         {
